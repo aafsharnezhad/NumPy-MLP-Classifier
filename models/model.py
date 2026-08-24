5e2d75426c79fcea4ea9d65e7dd05761a8c9d4c6
Ali Afsharnezhad <a.afsharnezhad80@gmail.com>
2026-08-24 14:39:53 +0330

    base of model.py completed

--- models/model.py
+++ models/model.py
@@ -17,14 +17,7 @@ def soft_max(Z):
     return predicted_class
 
 
-# class OutPutLayer():
-#     def __init__(self, A_prev, num_classes):
-#         input_dim = A_prev.shape()[1]
-#         self.w = np.random.rand(input_dim, neurons)
-#         self.b = np.zeros(shape=(1, neurons))
-#         self.dw = 0
-#             self.dz = 0
-    
+  
 def cross_entropy_loss(y_pred, y_true):
     # Extracting Batch Size
     B = y_pred.shape[0]
@@ -49,21 +42,22 @@ def relu_backward(x):
 
 
 class DenseLayer():
-    def __init__(self,A_prev, neurons):
+    def __init__(self,input_dim, neurons):
 
         self.neurons = neurons
-        input_dim = A_prev.shape()[1]
+        
         self.W = np.random.rand(input_dim, neurons)
         self.b = np.zeros(shape=(1, neurons))
         self.dw = 0
         self.dz = 0
-        self.batch_size = 64
+        
         self.cache = {}
         self.V_dW = np.zeros_like(self.W)
         self.V_db = np.zeros_like(self.b)
 
     def forward(self, A_prev):
         z = A_prev @ self.W + self.b # [B, first_layer]
+        self.cache['current_z'] = z
         self.cache['A_prev'] = A_prev 
         return z 
 
@@ -105,44 +99,75 @@ class DenseLayer():
 class FeedForwardNeuralNetwork():
     def __init__(self,layers, X, Y_ture, batch_size = 32, num_classes = 10):
         self.first_layer_neurons = layers[0]
-        self.second_layer_neurons = layers[1]
+
+
+        self.y_true = Y_ture 
         self.num_classes = num_classes
-        self.w1 = np.random.randn(784, self.first_layer_neurons)
-        self.b1 = np.zeros(shape=(1,self.first_layer_neurons))
+
+        self.model_layers = []
+
+
+        ### example : layers = [16,32] ----> model_layers = [DenseLayer, Dense_layer, Dense_layer]
+        for i in range(len(layers)) : 
+            if i == 0 :
+                input_dim = 784
+            else : 
+                input_dim = layers[i-1]    
+
+            self.model_layers.append(DenseLayer(input_dim=input_dim , neurons = layers[i]))
+        # Creating Last hidden Layer    
+        self.model_layers.append(DenseLayer(input_dim=layers[-1], neurons=num_classes))
+
+
+
+        # self.w1 = np.random.randn(784, self.first_layer_neurons)
+        # self.b1 = np.zeros(shape=(1,self.first_layer_neurons))
+
+
         self.Y_true = Y_ture
+
         self.X = X
 
-        if layers[1] is not None :
-            second_layer_neurons = layers[1]
-            self.w2 = np.random.randn(self.first_layer_neurons, second_layer_neurons)
-            self.b2 = np.zeros(shape=(1,second_layer_neurons))
+        # if len(layers) > 2 :
+        #     second_layer_neurons = layers[1]
+        #     # self.w2 = np.random.randn(self.first_layer_neurons, second_layer_neurons)
+        #     # self.b2 = np.zeros(shape=(1,second_layer_neurons))
             
 
-        else : 
-            second_layer_neurons = layers[0]
+        # else : 
+        #     second_layer_neurons = layers[0]
 
                 
 
-        self.last_w = np.random.rand(second_layer_neurons, num_classes)
-        self.last_b = np.zeros(shape=(1,num_classes))
+        # self.last_w = np.random.rand(second_layer_neurons, num_classes)
+        # self.last_b = np.zeros(shape=(1,num_classes))
             
 
             
     def forward(self, X):
 
-        self.layer_1 = DenseLayer(A_prev = X, neurons = self.first_layer_neurons)
-        self.A_1, self.Z1 = self.layer_1.forward(A_prev = X)
-        self.layer_2 = DenseLayer(A_prev = self.A_1, neurons = self.second_layer_neurons)
-        self.A_2, self.Z2 = self.layer_2.forward(A_prev = self.A_1)
-        self.out_put_layer = DenseLayer(A_prev = self.A_2, neurons = self.num_classes)
-        self.y_predicted = self.out_put_layer.forward(A_prev = self.A_2)
+        A_prev = X
+        for i in range(len(self.model_layers) - 1):
+           layer =  self.model_layers[i]
+           Z = layer.forward(A_prev = A_prev)
+           A = relu(Z)
+           A_prev = A
+
+        layer = self.model_layers[-1]
+        Z_out = layer.forward(A_prev = A_prev)
+        A_out = soft_max(Z_out)   
         
+        self.y_predicted = A_out
+         
         
         return self.y_predicted  
 
 
-    def backward(self):
-        dz_last, dw_last = self.out_put_layer.backward(self,  A_prev = self.A_2, Y_true = self.Y_true, Y_predicted = self.y_predicted)
-        dz_2, dw_2 =  self.out_put_layer.backward(self, A_prev = self.A_1, w_next = self.last_w, current_z = self.Z2, dz_next = dz_last)
-        dz_1, dw_1 =  self.out_put_layer.backward(self, A_prev = self.X, w_next = self.w2, current_z = self.Z1, dz_next = dz_2)
-        return dw_last, dw_2, dw_1
+    def backward(self, learning_rate):
+        for i in reversed(range(len(self.model_layers)-1)):
+            layer = self.model_layers[i]
+            
+            current_z = layer.cache['current_z']
+            dZ = layer.backward(learning_rate, w_next=w_next, beta = 0.9, current_z=current_z, Y_true=self.y_true, Y_predicted=self.y_predicted, dz_next=dz_next, lambda_reg=0)
+            w_next = layer.W
+            dz_next = dZ
